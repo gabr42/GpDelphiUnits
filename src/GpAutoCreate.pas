e54a744a420b52efc86b21bcb71af351d3b7cc13
gabr42@gmail.com <gabr42@gmail.com@2e62f740-2fc3-8315-3d21-85ebbd0eea70>
2013-10-06 08:57:02 +0000

    DSiWin32 ! Fixed enumeration routines to not return folders that don't match the mask.
    GpAutoCreate
    + Implemented GpManagedDestroy attribute which handles only the destruction of the associated object.
    + TGpManaged is derived from TInterfacedObject.

--- src/GpAutoCreate.pas
+++ src/GpAutoCreate.pas
@@ -3,15 +3,20 @@
 ///</summary>
 ///<author>Primoz Gabrijelcic</author>
 ///<remarks><para>
-///   (c) 2012 Primoz Gabrijelcic
+///   (c) 2013 Primoz Gabrijelcic
 ///   Free for personal and commercial use. No rights reserved.
 ///
 ///   Author            : Primoz Gabrijelcic
 ///   Creation date     : 2012-10-28
-///   Last modification : 2012-10-29
-///   Version           : 1.01
+///   Last modification : 2013-06-06
+///   Version           : 1.03
 ///</para><para>
 ///   History:
+///     1.03: 2013-06-06
+///       - Implemented GpManagedDestroy attribute which handles only the destruction of
+///         the associated object.
+///     1.02: 2013-04-04
+///       - TGpManaged is derived from TInterfacedObject.
 ///     1.01: 2012-10-29
 ///       - Refactored creation/destruction code into class procedures that can be used
 ///         from any class as suggested by [Stefan Glienke].
@@ -25,31 +30,48 @@ unit GpAutoCreate;
 interface
 
 uses
-  System.Rtti;
+  System.Rtti,
+  System.Classes,
+  Vcl.Forms;
 
 type
   GpManagedAttribute = class(TCustomAttribute)
   public type
-    TConstructorType = (ctNoParam, ctParBoolean);
+    TConstructorType = (ctNoParam, ctParBoolean, ctParInteger);
   strict private
     FBoolParam      : boolean;
+    FIntParam       : integer;
     FConstructorType: TConstructorType;
   public
-    class function  IsManaged(const obj: TRttiNamedObject): boolean; static;
+    class function IsManaged(const obj: TRttiNamedObject): boolean; static;
     class function GetAttr(const obj: TRttiNamedObject; var ma: GpManagedAttribute): boolean; static;
     constructor Create; overload;
     constructor Create(boolParam: boolean); overload;
+    constructor Create(intParam: integer); overload;
     property BoolParam: boolean read FBoolParam;
     property ConstructorType: TConstructorType read FConstructorType;
-  end;
+    property IntParam: integer read FIntParam;
+  end; { GpManagedAttribute }
 
-  TGpManaged = class
+  GpManagedDestroyAttribute = class(TCustomAttribute)
+  public
+    class function IsManaged(const obj: TRttiNamedObject): boolean; static;
+    class function GetAttr(const obj: TRttiNamedObject; var ma: GpManagedDestroyAttribute): boolean; static;
+  end; { GpManagedDestroyAttribute }
+
+  TGpManaged = class(TInterfacedObject)
   public
     constructor Create;
     destructor  Destroy; override;
     class procedure CreateManagedChildren(parent: TObject); static;
     class procedure DestroyManagedChildren(parent: TObject); static;
-  end;
+  end; { TGpManaged }
+
+  TGpManagedForm = class(TForm)
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor  Destroy; override;
+  end; { TGpManagedForm }
 
 implementation
 
@@ -59,6 +81,12 @@ uses
 
 { GpManagedAttribute }
 
+constructor GpManagedAttribute.Create;
+begin
+  inherited Create;
+  FConstructorType := ctNoParam;
+end; { GpManagedAttribute.Create }
+
 constructor GpManagedAttribute.Create(boolParam: boolean);
 begin
   inherited Create;
@@ -66,10 +94,11 @@ begin
   FBoolParam := boolParam;
 end; { GpManagedAttribute.Create }
 
-constructor GpManagedAttribute.Create;
+constructor GpManagedAttribute.Create(intParam: integer);
 begin
   inherited Create;
-  FConstructorType := ctNoParam;
+  FConstructorType := ctParInteger;
+  FIntParam := intParam;
 end; { GpManagedAttribute.Create }
 
 class function GpManagedAttribute.GetAttr(const obj: TRttiNamedObject;
@@ -92,6 +121,29 @@ var
 begin
   Result := GetAttr(obj, ma);
 end; { GpManagedAttribute.IsManaged }
+
+{ GpManagedDestroyAttribute }
+
+class function GpManagedDestroyAttribute.GetAttr(const obj: TRttiNamedObject;
+  var ma: GpManagedDestroyAttribute): boolean;
+var
+  a: TCustomAttribute;
+begin
+  Result := false;
+  ma := nil;
+  for a in obj.GetAttributes do
+    if SameText(a.ClassName, 'GpManagedDestroyAttribute') then begin
+      ma := GpManagedDestroyAttribute(a);
+      Exit(true);
+    end;
+end; { GpManagedDestroyAttribute.GetAttr }
+
+class function GpManagedDestroyAttribute.IsManaged(const obj: TRttiNamedObject): boolean;
+var
+  ma: GpManagedDestroyAttribute;
+begin
+  Result := GetAttr(obj, ma);
+end; { GpManagedDestroyAttribute.IsManaged }
 
 { TGpManaged }
 
@@ -134,6 +186,13 @@ begin
         begin
           f.SetValue(parent, ctor.Invoke(f.FieldType.AsInstance.MetaclassType, [ma.BoolParam]));
           break; //for ctor
+        end
+        else if (ma.ConstructorType = GpManagedAttribute.TConstructorType.ctParInteger) and
+                (Length(params) = 1) and
+                (params[0].ParamType.Handle = TypeInfo(Integer)) then
+        begin
+          f.SetValue(parent, ctor.Invoke(f.FieldType.AsInstance.MetaclassType, [ma.IntParam]));
+          break; //for ctor
         end;
       end;
     end; //for ctor
@@ -150,16 +209,32 @@ begin
   ctx := TRttiContext.Create;
   t := ctx.GetType(parent.ClassType);
   for f in t.GetFields do begin
-    if not GpManagedAttribute.IsManaged(f) then
+    if not (GpManagedAttribute.IsManaged(f) or GpManagedDestroyAttribute.IsManaged(f)) then
       continue; //for f
     for dtor in f.FieldType.GetMethods('Destroy') do begin
       if dtor.IsDestructor then begin
-        dtor.Invoke(f.GetValue(parent), []);
-        f.SetValue(parent, nil);
+        if not f.GetValue(parent).IsEmpty then begin
+          dtor.Invoke(f.GetValue(parent), []);
+          f.SetValue(parent, nil);
+        end;
         break; //for dtor
       end;
     end; //for dtor
   end; //for f
 end; { TGpManaged.DestroyManagedChildren }
+
+{ TGpManagedForm }
+
+constructor TGpManagedForm.Create(AOwner: TComponent);
+begin
+  inherited;
+  TGpManaged.CreateManagedChildren(Self);
+end; { TGpManagedForm.Create }
+
+destructor TGpManagedForm.Destroy;
+begin
+  TGpManaged.DestroyManagedChildren(Self);
+  inherited;
+end; { TGpManagedForm.Destroy }
 
 end.
